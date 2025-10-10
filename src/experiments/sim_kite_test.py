3aa3ed9e1155ad1e2cab02811a1bc8d5c97325b5
rama0422 <rama042201@gmail.com>
2025-10-10 13:06:10 +0200

    Add modular turbine

---
+++ src/experiments/sim_kite_test.py
@@ -0,0 +1,88 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+from src.simulation.kite_model import Kite
+from src.utility.configs import rho, g, S, m, vol, dt, t_end
+
+
+
+# turbine = Turbine(r_turb, J_gen, T_gen_max, w_gen_max, w_gen_max_T, N_gear, eff_gear, kp, ki)
+kite = Kite(S, m, vol)
+
+
+# # time varying v_rel and w_ref functions
+# def v_rel_fun(t):
+#     return 5 + 1.5*math.sin(0.5*t)
+
+# def w_ref_fun(t):
+#     return 1500 / 60 * 2 * math.pi + 30*math.sin(0.5*t)
+
+
+# Simulation params
+v_current_i = np.array([2,0,0])
+F_turb = 0
+p0 = 0
+pdot0 = 0.2
+x0 = [p0, pdot0]
+
+sol = solve_ivp(lambda t, x: kite.kiteDynamics(t, x, v_current_i, F_turb), [0, t_end], x0, max_step = dt)
+# sol = solve_ivp(lambda t, x: turbine.turbineDynamics(t, x, v_rel_fun(t), w_ref_fun(t)), [0, t_end], x0, max_step = dt)
+
+print(sol)
+
+# plot
+ts = np.array(kite.data_log["ts"])
+rs = np.array(kite.data_log["r"])
+rs_p = np.array(kite.data_log["r_p"])
+rs_pp= np.array(kite.data_log["r_pp"])
+vs_kite_i = np.array(kite.data_log["v_kite_i"])
+vs_rel_i = np.array(kite.data_log["v_rel_i"])
+vs_rel_abs = np.array(kite.data_log["v_rel_abs"])
+alphas = np.array(kite.data_log["alpha"])
+Fs_aero_i = np.array(kite.data_log["Fs_aero"])
+Fs_grav_i = np.array(kite.data_log["Fs_grav"])
+Fs_buoy_i = np.array(kite.data_log["Fs_buoy"])
+Fs_tot_i = np.array(kite.data_log["Fs_tot"])
+Fs_thether = np.array(kite.data_log["Fs_thether"])
+
+
+# print(vs_kite_i.shape)
+fig, ax = plt.subplots(4,2, figsize=(15,9))
+
+ax[0,0].plot(sol.t, sol.y[0])
+# ax[0].plot(sol.t, np.ones(len(sol.t)) * w_ref)
+ax[0,0].set_title(r"$p(t)$")
+ax[0,0].legend([r"$p$"])
+ax[0,0].set_xlabel("Time [s]")
+
+ax[1,0].plot(sol.t, sol.y[1])
+ax[1,0].set_title(r"$\dot{p}(t)$")
+ax[1,0].set_xlabel("Time [s]")
+
+ax[2,0].plot(ts, np.linalg.norm(vs_kite_i, axis=1))
+ax[2,0].set_title("Kite speed over time")
+ax[2,0].set_xlabel("Time [s]")
+
+# plt.subplot(5,1,4)
+ax[3,0].plot(ts, alphas)
+ax[3,0].set_title("Angle of attack over time")
+ax[3,0].set_xlabel("Time [s]")
+
+ax[0,1].plot(ts, np.linalg.norm(Fs_aero_i, axis=1))
+ax[0,1].plot(ts, np.linalg.norm(Fs_buoy_i, axis=1))
+ax[0,1].plot(ts, -np.linalg.norm(Fs_grav_i, axis=1))
+ax[0,1].set_title(r"Aerodynamic, buoyancy and gravity forces over time")
+ax[0,1].legend([r"$F_{aero}$", r"$F_{buoy}$", r"$F_{grav}$"])
+ax[0,1].set_xlabel("Time [s]")
+
+ax[1,1].plot(ts, Fs_thether)
+ax[1,1].set_title("Thether force over time")
+ax[1,1].set_xlabel("Time [s]")
+
+fig.tight_layout(pad=1.0)
+
+
+plt.tight_layout()
+plt.show()
--- src/simulation/kite_model.py
+++ src/simulation/kite_model.py
@@ -1,6 +1,7 @@
-from src.utility.configs import rho
 import math
 import numpy as np
+
+from src.utility.configs import rho, g
 
 
 def path(p):
@@ -52,20 +53,24 @@ class Kite:
         # self.F_buoy = np.array([0,0,0])
         # self.F_hydro = np.array([0,0,0])
         # self.F_net = np.array([0,0,0])
+        self.F_thether = 0
 
-        # Data logging
+        # Data logging #TODO: as np arrays for efficiency? give steps using dt and t_end to class
         self.data_log = {"ts": [],
+                         "r": [],
+                         "r_p": [],
+                         "r_pp": [],
+                         "v_kite_i": [],
+                         "v_rel_i": [],
+                         "v_rel_abs": [],
+                         "alpha": [],
                          "Fs_aero": [],
                          "Fs_grav": [],
                          "Fs_buoy": [],
-                         "Fs_hydro": [],
-                         "Fs_net": []}
+                         "Fs_tot": [],
+                         "Fs_thether": []}
 
-        
-    def kiteDynamics(self, t, x, v_current_i):
-        p = x[0]
-        pdot = x[1]
-
+    def kinematics(self, p):
         r = path(p)
         r_p = path_p(p)
         r_pp = path_pp(p)
@@ -83,8 +88,10 @@ class Kite:
         # R_si = np.array([e1,e2,e3])
         R_si = np.column_stack([e1, e2, e3])
 
-        # print(R_si)
+        return r, r_p, r_pp, e1, e2, e3, R_si
 
+
+    def relativeVelocity(self, pdot, r_p, R_si, v_current_i):
         # Relative water velocity
         v_kite_i = r_p * pdot
         v_rel_i = v_current_i - v_kite_i
@@ -100,27 +107,141 @@ class Kite:
         # Forces in current frame
         alpha = alpha_ic #TODO
 
-        F_L = 0.5 * rho * v_rel_abs**2 * S * C_L(alpha)
-        F_D = 0.5 * rho * v_rel_abs**2 * S * C_D(alpha)
+        return v_kite_i, v_rel_i, v_rel_s, v_rel_c, v_rel_abs, alpha
+    
+
+    def accleration(self, pdot, r_p, r_pp, e1, e3, R_si, v_rel_abs, alpha, F_turb = 0):
+        F_L = 0.5 * rho * v_rel_abs**2 * self.S * C_L(alpha)
+        F_D = 0.5 * rho * v_rel_abs**2 * self.S * C_D(alpha)
 
         F_aero_c = np.array([-F_D, 0, -F_L])
 
-        F_aero_s = R_sc(-alpha_ic).T @ F_aero_c #TODO: check alpha sign -/+??
+        F_aero_s = R_sc(-alpha).T @ F_aero_c #TODO: check alpha sign -/+??
 
         F_aero_i = R_si @ F_aero_s
 
         # Forces in inertial frame
         g_i = np.array([0, 0, -g])
-        F_mg = m * g_i # gravity force
+        F_mg_i = self.m * g_i # gravity force
 
         z_i = np.array([0 ,0, 1])
-        F_b = rho * vol * g * z_i # buoyancy force
+        F_b_i = rho * self.vol * g * z_i # buoyancy force
 
-        F_tot_i = F_aero_i + F_mg + F_b # thether force unknown here
+        # turbine force
+        F_turb_s = np.array([-F_turb, 0, 0])
+        F_turb_i = R_si @ F_turb_s
+
+        F_tot_i = F_aero_i + F_mg_i + F_b_i + F_turb_i # thether force unknown here
 
         # print(F_aero_i, F_mg, F_b)
 
         #
-        pdotdot = (np.dot(e1, F_tot_i) - m * (np.dot(e1, r_pp) * pdot**2)) / (m * np.linalg.norm(r_p))
+        pdotdot = (np.dot(e1, F_tot_i) - self.m * (np.dot(e1, r_pp) * pdot**2)) / (self.m * np.linalg.norm(r_p))
 
-        F_thether = m * (np.dot(e3, r_pp) * pdot**2) - np.dot(e3, F_tot_i)
+        F_thether = self.m * (np.dot(e3, r_pp) * pdot**2) - np.dot(e3, F_tot_i)
+
+        return pdotdot, F_aero_i, F_mg_i, F_b_i, F_tot_i, F_thether
+        
+
+    # used only if kite used alone 
+    def kiteDynamics(self, t, x, v_current_i, F_turb = 0):
+        p = x[0]
+        pdot = x[1]
+
+        r, r_p, r_pp, e1, e2, e3, R_si = self.kinematics(p)
+
+        v_kite_i, v_rel_i, v_rel_s, v_rel_c, v_rel_abs, alpha = self.relativeVelocity(pdot, r_p, R_si, v_current_i)
+
+        pdotdot, F_aero_i, F_mg_i, F_b_i, F_tot_i, F_thether = self.accleration(pdot, r_p, r_pp, e1, e3, R_si, v_rel_abs, alpha, F_turb)
+        self.F_thether = F_thether
+
+        # data logging
+        self.data_log["ts"].append(t)
+        self.data_log["r"].append(r)
+        self.data_log["r_p"].append(r_p)
+        self.data_log["r_pp"].append(r_pp)
+        self.data_log["v_kite_i"].append(v_kite_i)
+        self.data_log["v_rel_i"].append(v_rel_i)
+        self.data_log["v_rel_abs"].append(v_rel_abs)
+        self.data_log["alpha"].append(alpha)
+        self.data_log["Fs_aero"].append(F_aero_i)
+        self.data_log["Fs_grav"].append(F_mg_i)
+        self.data_log["Fs_buoy"].append(F_b_i)
+        self.data_log["Fs_tot"].append(F_tot_i)
+        self.data_log["Fs_thether"].append(F_thether)
+
+        return [pdot, pdotdot]
+    
+    # def kiteDynamics(self, t, x, v_current_i, v_rel):
+    #     p = x[0]
+    #     pdot = x[1]
+
+    #     r = path(p)
+    #     r_p = path_p(p)
+    #     r_pp = path_pp(p)
+
+    #     # TODO: pre calculate and use as functions
+    #     # Stability basis
+    #     e1 = r_p / np.linalg.norm(r_p)
+    #     e3 = -r / np.linalg.norm(r)
+    #     e2 = np.linalg.cross(e3,e1) #TODO or reverse?
+    #     e2 /= np.linalg.norm(e2) # Does it need to be normalized?
+
+    #     # print(e1, e2, e3)
+
+    #     # Rot matrices 
+    #     # R_si = np.array([e1,e2,e3])
+    #     R_si = np.column_stack([e1, e2, e3])
+
+    #     # print(R_si)
+
+    #     # Relative water velocity
+    #     v_kite_i = r_p * pdot
+    #     v_rel_i = v_current_i - v_kite_i
+
+    #     v_rel_s = R_si.T @ v_rel_i
+
+    #     alpha_ic = np.atan2(-v_rel_s[2], -v_rel_s[0])# angle between axis e1 and v_rel_s
+    #     # print(v_rel_s)
+    #     v_rel_c = np.array([math.sqrt(v_rel_s[2]**2 + v_rel_s[0]**2), 0, 0])
+
+    #     v_rel_abs = v_rel_c[0]
+
+    #     # Forces in current frame
+    #     alpha = alpha_ic #TODO
+
+    #     F_L = 0.5 * rho * v_rel_abs**2 * self.S * C_L(alpha)
+    #     F_D = 0.5 * rho * v_rel_abs**2 * self.S * C_D(alpha)
+
+    #     F_aero_c = np.array([-F_D, 0, -F_L])
+
+    #     F_aero_s = R_sc(-alpha_ic).T @ F_aero_c #TODO: check alpha sign -/+??
+
+    #     F_aero_i = R_si @ F_aero_s
+
+    #     # Forces in inertial frame
+    #     g_i = np.array([0, 0, -g])
+    #     F_mg_i = self.m * g_i # gravity force
+
+    #     z_i = np.array([0 ,0, 1])
+    #     F_b_i = rho * self.vol * g * z_i # buoyancy force
+
+    #     F_tot_i = F_aero_i + F_mg_i + F_b_i # thether force unknown here
+
+    #     # print(F_aero_i, F_mg, F_b)
+
+    #     #
+    #     pdotdot = (np.dot(e1, F_tot_i) - self.m * (np.dot(e1, r_pp) * pdot**2)) / (self.m * np.linalg.norm(r_p))
+
+    #     F_thether = self.m * (np.dot(e3, r_pp) * pdot**2) - np.dot(e3, F_tot_i)
+    #     self.F_thether = F_thether
+
+    #     # data logging
+    #     self.data_log["ts"].append(t)
+    #     self.data_log["Fs_aero"].append(F_aero_i)
+    #     self.data_log["Fs_grav"].append(F_mg_i)
+    #     self.data_log["Fs_buoy"].append(F_b_i)
+    #     self.data_log["Fs_net"].append(F_tot_i)
+
+
+    #     return [pdot, pdotdot]
